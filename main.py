1816b709e84246f830d2b261de38476ab23612b8
Mar Garza C <harper.dagger.07@gmail.com>
2024-04-16 13:19:08 -0600

    Update
    Estoy viendo nuevas formas de acceder a la web, tengo varias librerías por si se necesitan a lo largo del código pero eventualmente quitaré las que no utilicemos. Tenemos la intención de utilizar selenium pero tenemos un problema con el time de la página abierta

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
+# Importar librerías
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait # No regresar operaciones hasta que cargue el HTML
+from selenium.webdriver.support import expected_conditions # 
+from selenium.webdriver.common.by import By 
+
+import time
+import pandas
 import requests
 from bs4 import BeautifulSoup
 
 def main():
-    print("Hello World")
+    # Abrir ZINC en Chrome
+    options = webdriver.ChromeOptions()
+    options.add_argument('--start-maximized')
+    options.add_argument('--disable-extensions')
 
+    driver = webdriver.Chrome(options=options)
+
+    driver.get('https://zinc.docking.org')
 if __name__ == "__main__":
     main()
