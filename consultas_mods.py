f8b426bfffe3f96276f6cb5ff25515e4884b95b6
Mar Garza C <harper.dagger.07@gmail.com>
2024-05-06 10:32:19 -0600

    OFICIAL

--- consultas_mods.py
+++ consultas_mods.py
@@ -3,6 +3,8 @@ import os
 import requests
 import openpyxl
 import matplotlib as plt 
+from openpyxl import load_workbook
+import pandas as pd
 
 def normalize(s):
     replacements = (
@@ -31,23 +33,45 @@ def menu():
     try:
         opcion = int(opcion)
         if opcion == 1:
-            url_base = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
-            consulta = ConsultaAPI(url_base)
+            try:
+                url_base = "https://pubchem.ncbi.nlm.nih.gov/rest/pug"
+                consulta = ConsultaAPI(url_base)
 
-            comp_name = input("Ingrese el nombre del compuesto a buscar: ")
-            # Funcion que quita tildes y regresa en lower case
-            comp_name = normalize(comp_name)
-            cid = consulta.realizar_consulta(comp_name)
+                comp_name = input("Ingrese el nombre del compuesto a buscar: ")
+                # Funcion que quita tildes y regresa en lower case
+                comp_name = normalize(comp_name)
+                cid = consulta.realizar_consulta(comp_name)
 
-            data_json = obtener_informacion_compuesto(cid)
-            if data_json is None:
-                print("Archivo no guardado")
+                data_json = obtener_informacion_compuesto(cid)
+                if data_json is None:
+                    print("Archivo no guardado")
+                else:
+                    comp_name_list.append(comp_name)
+                    data_str = json.dumps(data_json, indent=4)
+                    with open(str(comp_name)+".txt", "w") as file:
+                        file.write(data_str)
+                    print(f"Archivo {comp_name} guardado con éxito!")
+
+            except requests.exceptions.RequestException:
+                print("No se puede conectar a Internet. Por favor, verifica tu conexión y vuelve a intentarlo.")
+                if len(comp_name_list) > 0:
+                    print("O consulta tus archivos anteriormente guardados.")
+                    j = 0
+                    for i in comp_name_list:
+                        j += 1
+                        print("f{j}" ,i)
+        elif opcion == 2:
+            if len(comp_name_list) > 0:
+                pd.set_option('display.max_rows', None)
+                pd.set_option('display.max_columns', None)
+                for i in comp_name_list:
+                    with open (str(i)+".txt"):        
+                        data = pd.read_json(str(i)+".txt")
+                    print(data)
             else:
-                comp_name_list.append(comp_name)
-                data_str = json.dumps(data_json, indent=4)
-                with open(str(comp_name)+".txt", "w") as file:
-                    file.write(data_str)
-                print(f"Archivo {comp_name} guardado con éxito!")
+                print("Aun no hay archivos descargados.")
+            #abrir_xlsx(comp_name_list)
+        
         elif opcion == 5:
             borrar_todo()
         elif opcion == 6:
